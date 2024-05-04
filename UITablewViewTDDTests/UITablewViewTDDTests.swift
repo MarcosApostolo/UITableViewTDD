6ea38bd39c3321903ba68b418cee19d23d0f355b
Marcos Apostolo <marcos.vi.am.ap@gmail.com>
2024-05-04 08:53:23 -0300

    Initial commit

---
+++ UITablewViewTDDTests/UITablewViewTDDTests.swift
@@ -0,0 +1,12 @@
+//
+//  UITablewViewTDDTests.swift
+//  UITablewViewTDDTests
+//
+//  Created by Marcos Amaral on 04/05/24.
+//
+
+import XCTest
+
+final class UITablewViewTDDTests: XCTestCase {
+    
+}
